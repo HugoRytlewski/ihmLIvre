ba4488484bf2d787fda572ff916977a0d0a0083c
Hugo <rytlewski.hugo@etu.educ-valadon-limoges.fr>
2023-11-23 17:20:35 +0100

    first commit

---
+++ src/main/java/controller/AjouterControlleur.java
@@ -0,0 +1,21 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class AjouterControlleur {
+
+    @FXML
+    private Button ButtonAdd;
+
+    @FXML
+    private TextField inputAddLivreAuteur;
+
+    @FXML
+    private TextField inputAddLivreISBM;
+
+    @FXML
+    private TextField inputAddLivreTittle;
+
+}
